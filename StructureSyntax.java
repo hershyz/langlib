29ec74edcf1c8f2c3ee7730187a05db93a62c913
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-04-18 18:47:50 -0400

    Create StructureSyntax.java

---
+++ StructureSyntax.java
@@ -0,0 +1,9 @@
+public class StructureSyntax {
+
+    //Syntax for initializing if, else, for, while, and function statements:
+    public static String ifSyntax = "if";
+    public static String elseSyntax = "else";
+    public static String forSyntax = "for";
+    public static String whileSyntax = "while";
+    public static String functionSyntax = "func";
+}
