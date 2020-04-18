19944a25ccd036bd6a295341616ab0ce74d22e07
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-04-18 18:33:04 -0400

    Create VariableSyntax.java

---
+++ VariableSyntax.java
@@ -0,0 +1,8 @@
+public class VariableSyntax {
+
+    //Syntax for initializing variables, these values can be modified:
+    public static String stringSyntax = "string";
+    public static String doubleSyntax = "double";
+    public static String intSyntax = "int";
+    public static String boolSyntax = "bool";
+}
